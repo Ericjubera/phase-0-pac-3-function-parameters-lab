553417b5c31b6f54400af56e1d73c1e67ae925f6
eric jubera <ericjubera@gmail.com>
2022-07-18 16:05:06 -0500

    commit tree 0e1911fc4687b4ed37822ef757c07d8dd26329c8

---
+++ index.js
@@ -0,0 +1,12 @@
+function introduction(Aki) {
+    return(`Hi, my name is ${Aki}.`);
+}
+function introductionWithLanguage(Aki,Ember) {
+    return(`Hi, my name is ${Aki} and I am learning to program in ${Ember}.`) 
+}
+function introductionWithLanguageOptional(Gracie,u="JavaScript") {
+    return(`Hi, my name is ${Gracie} and I am learning to program in ${u}.`)
+
+    
+
+}
